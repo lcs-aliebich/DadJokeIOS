5cc8ab792c97059d770c5513cc80dd86661cf83f
Andreas Liebich <aliebich@lcs.on.ca>
2020-12-08 14:31:54 -0500

    Initial Commit

---
+++ DadJokeIOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DadJokeIOS
+//
+//  Created by Andreas Liebich on 2020-12-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DadJokeIOS/DadJokeIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DadJokeIOSApp.swift
+//  DadJokeIOS
+//
+//  Created by Andreas Liebich on 2020-12-08.
+//
+
+import SwiftUI
+
+@main
+struct DadJokeIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
